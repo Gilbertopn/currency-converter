dd7eabf0f3d18cd7f655c46a485eff38c0b918a1
Gilberto Junior <juninhossj4@gmail.com>
2026-02-18 17:02:11 -0300

    add domain validations and business rules

--- currency_converter/src/main/java/com/gilberto/currency_converter/domain/valueobject/Currency.java
+++ currency_converter/src/main/java/com/gilberto/currency_converter/domain/valueobject/Currency.java
@@ -1,11 +1,24 @@
 package com.gilberto.currency_converter.domain.valueobject;
 
+import java.util.Set;
 
 public class Currency {
+
+    private static final Set<String> SUPPORTED_CURRENCIES = Set.of("USD", "EUR", "BRL");
 
     private final String code;
 
     public Currency(String code) {
+        if (code == null || code.isBlank()) {
+            throw new IllegalArgumentException("Currency cannot be null or empty");
+        }
+
+        code = code.toUpperCase();
+
+        if (!SUPPORTED_CURRENCIES.contains(code)) {
+            throw new IllegalArgumentException("Unsupported currency: " + code);
+        }
+
         this.code = code;
     }
 
--- currency_converter/src/main/java/com/gilberto/currency_converter/domain/valueobject/Money.java
+++ currency_converter/src/main/java/com/gilberto/currency_converter/domain/valueobject/Money.java
@@ -8,6 +8,15 @@ public class Money {
     private final Currency currency;
 
     public Money(BigDecimal amount, Currency currency) {
+
+        if (amount == null || amount.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException("Amount must be greater than zero");
+        }
+
+        if (currency == null) {
+            throw new IllegalArgumentException("Currency cannot be null");
+        }
+
         this.amount = amount;
         this.currency = currency;
     }
